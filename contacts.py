0a6e540ceaa5caa0b50285179da8bb3634fd22ae
py27 <40369150+py27@users.noreply.github.com>
2018-06-18 20:00:01 -0400

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -1,6 +1,6 @@
 contacts=[
 ['John','Doe','',' IL','john@email.com'],
-['Chad','Smith','345-856-236','Ann Arbor MI','chad@email.com'],
+['Chad','Smith','345-856-236',' MI','chad@email.com'],
 ['R','Kunt','874-736-4394','IL','rkunt@email.com'],
 ['Taylor','Martin','517-402-0911','CA','taylor@email.com'],
 ]
